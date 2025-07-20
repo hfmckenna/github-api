a1a70ac990e51f419c5e4dd3fa86381e860b8b14
Hugh McKenna <hfmckenna@outlook.com>
2025-07-20 13:36:12 +0100

    chore: Linting

--- src/data/api.types.ts
+++ src/data/api.types.ts
@@ -2,9 +2,9 @@
  * Optional query parameters that can be used with various search requests on the GitHub API
  * @see https://docs.github.com/en/rest/search/search?apiVersion=2022-11-28#search-repositories
  */
-export type  GitHubSearchOptions = {
-    sort?: string;
-    order?: string;
-    results?: number;
-    page?: number;
-}
+export type GitHubSearchOptions = {
+  sort?: string;
+  order?: string;
+  results?: number;
+  page?: number;
+};
